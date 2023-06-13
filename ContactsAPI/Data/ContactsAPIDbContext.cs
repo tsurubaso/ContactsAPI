f2a5097a0eb76e01c01ca3513e425b62513c5e95
tsurubaso <tsurubaso@hotmail.com>
2023-06-13 18:57:11 +0900

    Sadly not working but will work on that

---
+++ ContactsAPI/Data/ContactsAPIDbContext.cs
@@ -0,0 +1,18 @@
+﻿using ContactsAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ContactsAPI.Data
+{
+    public class ContactsAPIDbContext : DbContext
+    {
+        public ContactsAPIDbContext(DbContextOptions options) : base(options)
+        {
+
+
+        }
+
+        public int MyProperty { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+
+    }
+}
